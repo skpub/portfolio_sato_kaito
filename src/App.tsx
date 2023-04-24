abb8f321ccdd2aefc840029f93dbd27436d98023
skpub <70882570+skpub@users.noreply.github.com>
2023-04-24 22:17:25 +0900

    Refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 /** @jsxImportSource @emotion/react */
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import { css } from '@emotion/react'
 import './App.css'
 import Header from './Header'
@@ -17,7 +15,7 @@ function App() {
   }
 
   const onScroll = () => {
-    document.getElementById("object")?.scrollIntoView({behavior: 'smooth'})
+    document.getElementById("object")?.scrollIntoView({ behavior: 'smooth' })
     // const scrollY = window.pageYOffset
   }
 
@@ -27,51 +25,31 @@ function App() {
 
 
 
-  const Title=css`
+  const Title = css`
     padding: 100%;  
   `
-  const blueblack='#303045'
+  const blueblack = '#303045'
 
   return (
     <>
-      <div id="Wrapper" css={css`
+      <div id="BackGround" css={css`
         position: relative;
         background: url({kaito}) no-repeat;
         background-size: cover;
       `}>
         <img css={css`
           width: 100vw;
-          filter: brightness(140%);
+          filter: brightness(130%);
         `} src={kaito} alt="" />
 
-        <div css={css`
-          display: flex;
-          width: 100vw;
-          color: ${blueblack};
-          background: #303030;
+        <Header />
+
+        <div id="Wrapper" css={css`
           position: absolute;
           top: 0px;
-          /* left: 55vw; */
           right: 0px;
           margin: auto;
-          /* h1 {
-            font-family: 'Shippori Mincho', serif;
-            font-size: 6vw;
-            margin: 0;
-            padding: 0;
-          } */
-          p {
-            font-size: 24px;
-            color: white;
-            margin: auto;
-          }
-          #Header-right {
-            a {
-              color: white;
-            }
-          }
         `}>
-          <Header />
         </div>
       </div>
     </>
--- src/Header.tsx
+++ src/Header.tsx
@@ -1,24 +1,51 @@
 /** @jsxImportSource @emotion/react */
 import React from 'react'
 import { css } from '@emotion/react'
-import arrow from '/img/arrow.svg'
 import { FaGithub, FaTwitter, FaFacebook } from 'react-icons/fa'
 
 export default class Header extends React.Component {
   render() {
     return (
-      <div>
-        <div id="Header-right" css={css`display: flex; background-color: blue; width: 200px; margin: 0 0 0 auto;`}>
+      <div css={css`
+        display: flex;
+        position: absolute;
+        top: 0px;
+        right: 0px;
+        color: #303045;
+        flex-wrap: wrap;
+        justify-content: flex-start;
+        width: 100vw;
+      `}>
+        <div id="Header-left" css={css`
+          padding-left: 10px;
+          font-family: 'Shippori Mincho', serif;
+          font-size: 24px;
+          padding: 0;
+          p {
+            margin: 0;
+            padding-left: 10px;
+          }
+        `}>
+          <p>Portfolio</p>
+        </div>
+        <div id="Header-right" css={css`
+          display: flex;
+          width: 200px;
+          margin: 0 0 0 auto;
+          font-size: 18px;
+          a {
+            color: #303045;
+          }
+        `}>
           <a href="https://github.com/skpub" css={css`
             display: block;
             margin-right: auto;
             margin-left: auto;
           `}>
             <FaGithub css={css`
-              font-size: 24px;
               /* color: #171515; */
               padding: 10px;
-            `}/>
+            `} />
           </a>
           <a href="https://twitter.com/OMGR_dearinsu" css={css`
             display: block;
@@ -26,10 +53,9 @@ export default class Header extends React.Component {
             margin-left: auto;
           `}>
             <FaTwitter css={css`
-              font-size: 24px;
               /* color: #1da1f2; */
               padding: 10px;
-            `}/>
+            `} />
           </a>
           <a href="https://www.facebook.com/profile.php?id=100085236288723" css={
             css`display: block;
@@ -37,11 +63,10 @@ export default class Header extends React.Component {
             margin-left: auto;
           `}>
             <FaFacebook css={css`
-              font-size: 24px;
               /* color: #3B5998; */
               padding: 10px;
-            `}/>
-            </a>
+            `} />
+          </a>
         </div>
       </div>
     )
