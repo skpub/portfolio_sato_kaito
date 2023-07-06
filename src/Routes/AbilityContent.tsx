46029f8c6063891e8c24f14505b783e374791db1
skpub <70882570+skpub@users.noreply.github.com>
2023-07-06 22:24:27 +0900

    できることとしてネットワーク関係を追加

--- src/Routes/AbilityContent.tsx
+++ src/Routes/AbilityContent.tsx
@@ -3,7 +3,7 @@ import { css } from '@emotion/react'
 
 export type AbilityProp = {
   title : string,
-  description: string,
+  description: [string, string[]][],
   img: [string, string][]
 }
 
@@ -21,11 +21,36 @@ export const AbilityContent = (props: AbilityProp) => {
         padding: 24px;
         font-family: 'Zen Maru Gothic';
       `}>{props.title}</h2>
-      <p>{props.description}</p>
+      <div css={css`
+        text-align: left;
+      `}>
+        {props.description.map((pair) => {
+          return (
+            <>
+            <h3 css={css`
+              font-family: 'Zen Maru Gothic';
+            `}>{pair.at(0)}</h3>
+            {pair[1].map((text) => {
+              return (
+                <>
+                <p css={css`
+                  padding-left: 1em;
+                `}>{text}</p>
+                </>
+              )
+            })}
+            </>
+          )
+        })
+        }
+      </div>
       <div css={css`
         display: flex;
         @media screen and (max-width: 520px) {
           flex-flow: column;
+          img {
+            aspect-ratio: unset;
+          }
         }
       `}>
         {props.img.map((pair) => {
@@ -36,8 +61,9 @@ export const AbilityContent = (props: AbilityProp) => {
               margin: 3vw;
             `}>
               <img src={pair.at(1)} alt="" css={css`
+                flex: 1;
                 width: 100%;
-                height: 60%;
+                aspect-ratio: 1;
                 object-fit: contain;
               `} />
               <p>{pair.at(0)}</p>
